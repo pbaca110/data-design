f98cd5e2bb56204b3ecc1c70a11916f99b094f02
Paul Baca <pbaca110@cnm.edu>
2017-04-13 08:31:47 -0600

    made files

--- epic/persona.php
+++ epic/persona.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: paully
- * Date: 4/12/17
- * Time: 4:57 PM
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>PERSONA</title>
+	</head>
+	<body>
+
+		<h1>NAME:</h1>
+		<P>NPaul Baca</P>
+	</body>
+</
+
