7efb6659ab049b3fca33cf5831cf29d3e5eac44c
Paul Baca <pbaca110@cnm.edu>
2017-04-19 16:25:17 -0600

    fixed sql file @rlewis2892

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -4,38 +4,37 @@ DROP TABLE IF EXISTS profile;
 
 
 
-CREATE TABLE profile (profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+CREATE TABLE profile (
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileActivationToken CHAR(32),
-	profileAtHandle VARCHAR(32)NOT NULL,
+	profileAtHandle VARCHAR(32) NOT NULL,
 	profileEmail VARCHAR(128) UNIQUE NOT NULL,
-profileHash CHAR(128) NOT NULL,
-profileSalt CHAR(64) NOT NULL,
+	profileHash CHAR(128) NOT NULL,
+	profileSalt CHAR(64) NOT NULL,
 	profilePhone VARCHAR(32),
-PRIMARY KEY(profileId)
+	PRIMARY KEY(profileId)
 );
 
 CREATE TABLE product (
-	productDate DATETIME NOT NULL,
-	productDescription INT UNSIGNED NOT NULL,
 	productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-productName VARCHAR(140) NOT NULL,
-INDEX(productId),
-FOREIGN KEY(productId) REFERENCES profile(profileId),
-PRIMARY KEY(productId)
+	productDate DATETIME NOT NULL,
+	productDescription VARCHAR(140) NOT NULL,
+	productName VARCHAR(64) NOT NULL,
+	INDEX (productName),
+	PRIMARY KEY(productId)
 );
-
 
 CREATE TABLE favorite (
 	favoriteDate DATETIME(6) NOT NULL,
-	favoriteProfileid INT UNSIGNED NOT NULL,
-favoriteProductid INT UNSIGNED NOT NULL ,
-INDEX(favoriteProfileid),
-INDEX(favoriteProfileid),
+	favoriteProfileId INT UNSIGNED NOT NULL,
+	favoriteProductId INT UNSIGNED NOT NULL,
+	INDEX(favoriteProfileId),
+	INDEX(favoriteProfileId),
 
-FOREIGN KEY (favoriteProfileId) REFERENCES profile(profileId),
-FOREIGN KEY (favoriteProductid) REFERENCES product(productId),
+	FOREIGN KEY (favoriteProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY (favoriteProductId) REFERENCES product(productId),
 
-PRIMARY KEY (FavoriteProfileId, favoriteProductId )
+	PRIMARY KEY (favoriteProfileId, favoriteProductId)
 );
 
 
